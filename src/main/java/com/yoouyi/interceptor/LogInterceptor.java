3e131bb718e9ea9402255686dbd4506d04eba9e6
zhaogangtao <zhaogangtao@gmail.com>
2014-11-07 19:01:26 +0800

    rename package name

--- src/main/java/com/yoouyi/interceptor/LogInterceptor.java
+++ src/main/java/com/yoouyi/interceptor/LogInterceptor.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LogInterceptor {
-	@Before("execution(* com.zgt..*.*(..))")
+	@Before("execution(* com.yoouyi..*.*(..))")
 	public void before(){
 		System.out.println("---aop before!");
 	}
